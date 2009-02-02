6704d93ecb7644329bc666ded7d8c8c31f004c70
Alexander Josef <22647518+alexander-josef@users.noreply.github.com>
2009-02-02 19:40:44 +0000

    generated scaffolded controllers and views

--- grails-app/controllers/UserController.groovy
+++ grails-app/controllers/UserController.groovy
@@ -1,50 +1,128 @@
-class UserController extends BaseController {
+class UserController {
 
-  def scaffold = User
+    def index = { redirect(action: list, params: params) }
+
+    // the delete, save and update actions only accept POST requests
+    def allowedMethods = [delete: 'POST', save: 'POST', update: 'POST']
 
 
-  /**
-   * Intercept all reqeusts with a login page, except the login and logout action
-   */
-  def beforeInterceptor = [action: this.&auth,
-          except: ['login', 'logout']]
+    /**
+     * Intercept all reqeusts with a login page, except the login and logout action
+     */
+    def beforeInterceptor = [action: this.&auth,
+            except: ['login', 'logout']]
 
-  /**
-   * Login closure: 
-   */
-  def login = {
-    if (request.method == "GET") {
-      session.userId = null
-      def user = new User()
+    /**
+     * Login closure:
+     */
+    def login = {
+        if (request.method == "GET") {
+            session.userId = null
+            def user = new User()
+        }
+        else if (request.method == "POST") {
+            def user = User.findByUserIdAndHashedPassword(params.userId, params.password)
+            if (user) {
+                session.userId = user.userId
+                println("succesfully logged in for user ${user}!")
+
+                // forward to original action and params
+                def redirectParams =
+                session.originalRequestParams ?
+                    session.originalRequestParams :
+                    [controller: 'wohnung']
+                redirect(redirectParams)
+
+            }
+            else {
+                flash['message'] = 'Please enter a valid user ID and password'
+            }
+        }
+        else {
+            flash['message'] = 'Unknown method!'
+
+        }
     }
-    else if (request.method == "POST") {
-      def user = User.findByUserIdAndHashedPassword(params.userId, params.password)
-      if (user) {
-        session.userId = user.userId
-        println("succesfully logged in for user ${user}!")
 
-        // forward to original action and params
-        def redirectParams =
-        session.originalRequestParams ?
-          session.originalRequestParams :
-          [controller: 'wohnung']
-        redirect(redirectParams)
 
-      }
-      else {
-        flash['message'] = 'Please enter a valid user ID and password'
-      }
+    def logout = {
+        session.userId = null
+        flash['message'] = 'Successfully logged out'
+        redirect(controller: 'race', action: 'search')
     }
-    else {
-      flash['message'] = 'Unknown method!'
 
+    def list = {
+        if (!params.max) params.max = 10
+        [userInstanceList: User.list(params)]
     }
-  }
 
+    def show = {
+        def userInstance = User.get(params.id)
 
-  def logout = {
-    session.userId = null
-    flash['message'] = 'Successfully logged out'
-    redirect(controller: 'race', action: 'search')
-  }
+        if (!userInstance) {
+            flash.message = "User not found with id ${params.id}"
+            redirect(action: list)
+        }
+        else { return [userInstance: userInstance] }
+    }
+
+    def delete = {
+        def userInstance = User.get(params.id)
+        if (userInstance) {
+            userInstance.delete()
+            flash.message = "User ${params.id} deleted"
+            redirect(action: list)
+        }
+        else {
+            flash.message = "User not found with id ${params.id}"
+            redirect(action: list)
+        }
+    }
+
+    def edit = {
+        def userInstance = User.get(params.id)
+
+        if (!userInstance) {
+            flash.message = "User not found with id ${params.id}"
+            redirect(action: list)
+        }
+        else {
+            return [userInstance: userInstance]
+        }
+    }
+
+    def update = {
+        def userInstance = User.get(params.id)
+        if (userInstance) {
+            userInstance.properties = params
+            if (!userInstance.hasErrors() && userInstance.save()) {
+                flash.message = "User ${params.id} updated"
+                redirect(action: show, id: userInstance.id)
+            }
+            else {
+                render(view: 'edit', model: [userInstance: userInstance])
+            }
+        }
+        else {
+            flash.message = "User not found with id ${params.id}"
+            redirect(action: edit, id: params.id)
+        }
+    }
+
+    def create = {
+        def userInstance = new User()
+        userInstance.properties = params
+        return ['userInstance': userInstance]
+    }
+
+    def save = {
+        def userInstance = new User(params)
+        if (!userInstance.hasErrors() && userInstance.save()) {
+            flash.message = "User ${userInstance.id} created"
+            redirect(action: show, id: userInstance.id)
+        }
+        else {
+            render(view: 'create', model: [userInstance: userInstance])
+        }
+    }
 }
