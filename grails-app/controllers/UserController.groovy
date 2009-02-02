ecdd9ad691535461d69ff0f2f7e5b535c3ff6b34
Alexander Josef <22647518+alexander-josef@users.noreply.github.com>
2009-02-02 07:36:06 +0000

    login

--- grails-app/controllers/UserController.groovy
+++ grails-app/controllers/UserController.groovy
@@ -1,4 +1,37 @@
 class UserController {
 
-    def scaffold = User
+  def scaffold = User
+
+  /**
+   * Login closure: 
+   */
+  def login = {
+    if (request.method == "GET") {
+      session.userId = null
+      def user = new User()
+    }
+    else if (request.method == "POST") {
+      def user = User.findByUserIdAndHashedPassword(params.userId, params.password)
+      if (user) {
+        session.userId = user.userId
+        println("succesfully logged in for user ${user}!")
+        
+        redirect(controller: 'user')
+      }
+      else {
+        flash['message'] = 'Please enter a valid user ID and password'
+      }
+    }
+    else {
+      flash['message'] = 'Unknown method!'
+
+    }
+  }
+
+
+  def logout = {
+    session.userId = null
+    flash['message'] = 'Successfully logged out'
+    redirect(controller: 'race', action: 'search')
+  }
 }
